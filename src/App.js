f8b0ce01e6b5dd391ae8826da861af20d5af2dc7
harzjunior <harunjibs@gmail.com>
2021-08-06 01:22:17 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import  './App.css';
+import Recipe from './Recipe';
 
-function App() {
-  return (
+const App = () => {
+  const APP__ID = 'befffe72';
+  const APP__KEY = 'ef72bd74f6daecceee6f0db23c0c404e';
+
+  const [recipes, setRecipes] = useState([]); 
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState ('chicken');
+
+  useEffect(() => {
+    getRecipes();
+  }, [query]);  
+
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${APP__ID}&app_key=${APP__KEY}`);
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
+  };
+
+  const  updateSearch = e => {
+    setSearch(e.target.value);
+    console.log(search);
+  };
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
+  }
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <form onSubmit={getSearch} className="search__form">
+        <input 
+        className="search__bar" 
+        type="text" 
+        value={search} 
+        onChange={updateSearch}
+        /> 
+        <button 
+        className="search__button" 
+        type="submit"
+        >Search</button>
+      </form>
+      <div className="recipes">
+      {recipes.map(recipes => (
+        <Recipe 
+        key={recipes.recipe.lebel}
+        title={recipes.recipe.label}
+        cuisineType={recipes.recipe.cuisineType}  
+        calories={recipes.recipe.calories}
+        healthLabels={recipes.recipe.healthLabels}
+        cautions={recipes.recipe.cautions}
+        image={recipes.recipe.image}
+        ingredients={recipes.recipe.ingredients}
+        foodCategory={recipes.recipe.ingredients.foodCategory}
+        />
+      ))}
+      </div>
+    </div>  
   );
-}
+};
+
 
 export default App;
---
+++ src/Recipe.js
@@ -0,0 +1,67 @@
+import React from "react";
+import Style from "./recipes.module.css";
+
+const Recipe = ({
+  title,
+  calories,
+  image,
+  ingredients,
+  cuisineType,
+  healthLabels,
+  cautions,
+}) => {
+  return (
+    <div className={Style.main}>
+      <div className={Style.main__title__content}>
+        <h1 className={Style.main__title}>{title}</h1>
+      </div>
+
+      <div className={Style.main__content}>
+        <div className={Style.main__leftSection}>
+          <div className={Style.main__leftSectionContent}>
+            <div className={Style.recipe__image}>
+              <img
+                src={image}
+                alt={title}
+                className={Style.recipe__imageContent}
+              />
+            </div>
+            
+            <div className={Style.recipe__caution}>
+            <p className={Style.caution}>Caution</p>
+              {cautions.map((cautions) => (
+                <li className={Style.recipe__cautionContent}>{cautions}</li>
+              ))}
+            </div>
+          </div>
+          <div className={Style.main__cuisineType}>
+          <p className={Style.cuisin}>Cuisin Type</p>
+            <h2>{cuisineType}</h2>
+          </div>
+          <div>
+            <ul className={Style.recipe__list}>
+              {ingredients.map((ingredients) => (
+                <li className={Style.recipe__ingredient}>{ingredients.text}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <div className={Style.main__righSection}>
+          <div className={Style.main__rightSectionContent}>
+            {healthLabels.map((healthLabels) => (
+              <li className={Style.recipe__healthLabels}>{healthLabels}</li>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <div className={Style.recipe__calory}>
+        <h3 className={Style.recipe__caloryContent}>
+          Total Calories: {calories}
+        </h3>
+      </div>
+    </div>
+  );
+};
+
+export default Recipe;
